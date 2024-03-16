6da8acc8fd703b1d356ef14f24e4967b15f583b8
Sorin37 <glizard.glizard@yahoo.com>
2024-03-16 23:03:05 +0200

    Got myself a respectable menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { NavigationMenuComponent } from "./navigation-menu/navigation-menu.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+        RouterOutlet,
+        ButtonModule,
+        NavigationMenuComponent
+    ]
 })
 export class AppComponent {
   title = 'doremi-app';
---
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navigation-menu',
+  standalone: true,
+  imports: [MenubarModule],
+  templateUrl: './navigation-menu.component.html',
+  styleUrl: './navigation-menu.component.scss',
+})
+export class NavigationMenuComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
+      },
+      {
+        label: 'Despre noi',
+        icon: 'pi pi-fw pi-pencil',
+      },
+      {
+        label: 'Evenimente',
+        icon: 'pi pi-fw pi-calendar',
+        routerLink: '/evenimente',
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-fw pi-user',
+      }
+    ];
+  }
+}
