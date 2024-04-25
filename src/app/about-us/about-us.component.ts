cb7f0e0405faba8895bbaca032ecf1e358ae3830
Sorin37 <glizard.glizard@yahoo.com>
2024-04-25 22:56:06 +0300

    Added location photos

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
+import { CarouselModule } from 'primeng/carousel';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [GoogleMapsModule, AccordionModule, ButtonModule],
+  imports: [GoogleMapsModule, AccordionModule, ButtonModule, CarouselModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
@@ -14,4 +15,33 @@ export class AboutUsComponent {
   location = new google.maps.LatLng(45.65697878480382, 25.625067447038685);
 
   options = { animation: google.maps.Animation.BOUNCE };
+
+  responsiveOptions = [
+    {
+      breakpoint: '1199px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '991px',
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '767px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  images = [
+    'Bookshelf.jpg',
+    'Entrance.jpg',
+    'HangerWelcome.jpg',
+    'MusicalInstruments.jpg',
+    'PaintingAndSlinky.jpg',
+    'TableWithBoard.jpg',
+    'TableWithPlanets.jpg',
+    'Toys.jpg',
+  ];
 }
