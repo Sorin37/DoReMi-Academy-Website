bbe7d38ffe94ecc45276a63738503822a063ab40
Sorin37 <glizard.glizard@yahoo.com>
2024-06-03 13:19:16 +0300

    Implemented a collapsable menu

--- src/app/navigation-menu/navigation-menu.component.ts
+++ src/app/navigation-menu/navigation-menu.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navigation-menu',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [MenubarModule, ButtonModule],
   templateUrl: './navigation-menu.component.html',
   styleUrl: './navigation-menu.component.scss',
 })
 export class NavigationMenuComponent {
+  closeMenu() {
+    let menu = document.getElementsByClassName("popup-menu-container")[0] as HTMLElement
+    menu.style.display = "none";
+  }
 
+  openMenu() {
+    let menu = document.getElementsByClassName("popup-menu-container")[0] as HTMLElement
+    menu.style.display = "block";
+  }
 }
